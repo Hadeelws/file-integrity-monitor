55e89483719fea8968e886ce580bd228eb240a00
Hadeel Saleh <hadeelsalwafi@gmail.com>
2026-08-13 00:16:52 +0300

    Fix file integrity monitoring

--- integrity_monitor.py
+++ integrity_monitor.py
@@ -1,36 +1,42 @@
 import hashlib
+import json
+from pathlib import Path
+BASELINE_FILE = "baseline.json"
+
 def calculate_hash(file_path):
+    """Calculate the SHA-256 hash of a file."""
     with open(file_path, "rb") as file:
-        file_data = file.read()
-    return hashlib.sha256(file_data).hexdigest()
+        return hashlib.sha256(file.read()).hexdigest()
+    
 def create_baseline(file_path):
     file_hash = calculate_hash(file_path)
     with open("baseline.txt", "w") as file:
-        file.write(file_hash)
+         file.write(file_hash)
     print("\nBaseline created successfully")
-    print("SHA-256:", file_hash)
-def check_integrity(file_path):
-    current_hash = calculate_hash(file_path)
-    with open("baseline.txt", "r") as file:
-        original_hash = file.read()
-    if current_hash == original_hash:
-        return True
-    else:
-        return False
+    print(f"SHA-256: {file_hash}") 
+
+def check_integrity(file_paths):
+   current_hash = calculate_hash(file_paths)
+   with open("baseline.txt", "r") as file:
+       original_hash = file.read().strip()
+   if current_hash == original_hash:
+       return True
+   else:
+       return False
 def main():
     print("File Integrity Monitor")
     print("-----------------------")
     print("1. Create Baseline")
     print("2. Check Integrity")
     choice = input("\nChoose an option: ")
-    file_path = input("Enter the file path: ")
     if choice == "1":
-       create_baseline(file_path)
+       files = input("Enter file paths spareted by commas: ").split(",")
+       files = [file.strip() for file in files if file.strip()]
+       create_baseline(files)
     elif choice == "2":
-         if check_integrity(file_path):
-                print("\nFile is unchanged.")
-         else:
-                print("\n ALERT: File has been modified!")
+         files = input("Enter current file paths separated by commas: ").split(",")
+         files = [file.strip() for file in files if file.strip()]
+         check_integrity(files)
     else:
          print("\nInvalid option.")
 
